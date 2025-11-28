966292375f69a0093b440885b30488a2d5bd74f5
Marek <marek1111.capkovic@gmail.com>
2025-11-28 11:26:54 +0100

    opravene cors

--- platba/vercelapi/api/create-checkout-session.js
+++ platba/vercelapi/api/create-checkout-session.js
@@ -3,6 +3,16 @@ import Stripe from 'stripe';
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "https://www.matustct.sk");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+
+  // Preflight request (OPTIONS)
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
 
   try {
